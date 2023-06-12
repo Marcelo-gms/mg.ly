a60ecf6f0f2711f7099e11551d95d3f924d83c2d
marcelo-gms <smarcello023@gmail.com>
2023-06-12 17:57:10 -0400

    resolve bug in js

--- js/app.js
+++ js/app.js
@@ -43,10 +43,10 @@ const fetchShortenerUrl = async (e) => {
   if (data) {
     showResponse();
     urlShortened.textContent = data.data.tiny_url;
+    inputUrlValue.value = "";
     return;
   }
 
-  inputUrlValue.value = "";
 };
 
 
